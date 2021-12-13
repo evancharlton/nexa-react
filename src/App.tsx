7200b31813bc636979c57e2a35b283149f79871e
Evan Charlton <evancharlton@microsoft.com>
2021-12-13 14:59:56 +0100

    Simple PoC to test CORS

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useState } from "react";
+import Axios from "axios";
 
 function App() {
+  const [ip, setIp] = useState("192.168.11.146");
+  const [nodes, setNodes] = useState<object | null>(null);
+  const [error, setError] = useState<Error>();
+
+  const listNodes = useCallback(() => {
+    Axios.get(`http://${ip}/v1/nodes`, {
+      headers: {
+        Authorization: `Digest username="nexa", realm="NexaRealm", nonce="SdtVF0h3VliycaRBQljGSsJ1Ie8mWNIW", uri="/v1/nodes", algorithm=MD5, response="efbbb0e8f48b1e4d8e1f47735d819cfe", opaque="asdasdh123u8123u1283u213213", qop=auth, nc=00000016, cnonce="2626b0fda5db2151"`,
+      },
+    })
+      .then((nodes) => {
+        console.log(nodes);
+        setNodes(nodes.data);
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(error);
+      });
+  }, [ip]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        placeholder="Bridge X IP / domain"
+        value={ip}
+        onChange={(e) => setIp(e.target.value)}
+      />
+      <button onClick={() => listNodes()}>List nodes</button>
+      <hr />
+      {nodes && (
+        <>
+          <h1>Nodes</h1>
+          <pre>{JSON.stringify(nodes, null, 2)}</pre>
+        </>
+      )}
+      {error && (
+        <>
+          <h1>{error.message}</h1>
+          <pre>{error.stack}</pre>
+        </>
+      )}
+    </>
   );
 }
 
